e3f1e763ff5a8076e178fceef3e2b3476936986a
Dennis Klyver <tripledk.stuff@jubii.dk>
2019-04-27 22:12:41 +0200

    Extra tile functionality

--- CollectMinerals/Assets/Scripts/EventManager.cs
+++ CollectMinerals/Assets/Scripts/EventManager.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public static class EventManager {
-    public static UnityEvent<int> onResourcePickup;
-    public static UnityEvent onPlayerDie;
-    public static UnityEvent onWinConditionAchieved;
-    public static UnityEvent onPlayerWin;
-    public static UnityEvent onPause;
-    public static UnityEvent onUnpause;
+public class UnityIntEvent : UnityEvent<int> { }
+
+public class EventManager : MonoBehaviour {
+    public UnityEvent<int> onResourcePickup = new UnityIntEvent ();
+    public UnityEvent onPlayerDie = new UnityEvent ();
+    public UnityEvent onWinConditionAchieved = new UnityEvent ();
+    public UnityEvent onPlayerWin = new UnityEvent ();
+    public UnityEvent onPause = new UnityEvent ();
+    public UnityEvent onUnpause = new UnityEvent ();
+
+    public static EventManager singleton;
+
+    void Start () {
+        if (singleton == null) {
+            singleton = this;
+        } else {
+            Debug.LogWarning ("Multiple instances of EventManager found!", this);
+        }
+    }
 
 }
