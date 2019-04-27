1a3fd7922b4eaa0ddfa23303d847433a700423c6
Michael Olsen <micdkhome@gmail.com>
2019-04-27 23:34:32 +0200

    Added running animation.

--- CollectMinerals/Assets/Scripts/PlayerController.cs
+++ CollectMinerals/Assets/Scripts/PlayerController.cs
@@ -3,48 +3,47 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;             //Floating point variable to store the player's movement speed.
-    //private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    public float speed; // Floating point variable to store the player's movement speed.
+    private Animator animator;
 
     // Use this for initialization
     void Start()
     {
-        ////Get and store a reference to the Rigidbody2D component so that we can access it.
-        //rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        ////moves player left right up down
-        //var x = Input.GetAxis("Horizontal") * Time.smoothDeltaTime * speed;
-        //var y = Input.GetAxis("Vertical") * Time.smoothDeltaTime * speed;
+        var isIdle = true;
 
         //W S Foward/Backward
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(0, speed * Time.deltaTime, 0, Space.World);
-            //Debug.Log("Does Wwork?");
+            isIdle = false;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(0, -speed * Time.deltaTime, 0, Space.World);
-            //Debug.Log("Does Swork?");
+            isIdle = false;
         }
 
         //A D side to side
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(-speed * Time.deltaTime, 0, 0, Space.World);
-            //Debug.Log("Does A work?");
+            transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+            isIdle = false;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(speed * Time.deltaTime, 0, 0, Space.World);
-            //Debug.Log("Does D work?");
+            transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+            isIdle = false;
         }
 
-        //rb2d.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
+        animator.SetBool("IsIdle", isIdle);
     }
 }
