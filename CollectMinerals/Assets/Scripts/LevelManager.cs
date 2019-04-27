fda1598c02faf69334c4e428147377050d9cba33
Dennis Klyver <tripledk.stuff@jubii.dk>
2019-04-27 18:06:17 +0200

    FMOD gitignore EventManager start

---
+++ CollectMinerals/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+    public int pointsToWin = 100;
+    public int currentPoints = 0;
+
+    // Start is called before the first frame update
+    void Start () {
+        EventManager.onResourcePickup.AddListener (CheckForWinCondition);
+    }
+
+    void CheckForWinCondition (int resourcesAdded) {
+        currentPoints += resourcesAdded;
+        if (resourcesAdded >= pointsToWin) {
+            EventManager.onWinConditionAchieved.Invoke ();
+        }
+    }
+
+}
